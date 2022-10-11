ee5066363176a9e5ab6b40b6d6ebb2e899884777
AmineD931 <59117151+kyu931@users.noreply.github.com>
2022-10-11 18:37:42 +0100

    reduce time to generate pdf document

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import puppeteer from "puppeteer";
 import cors from "cors";
 import PDFDocument from "pdfkit";
 import { convert } from "html-to-text";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -21,28 +22,19 @@ app.get("/", (req, res) => {
 
 app.post("/", async (req, res) => {
   const url = req.body.url;
-  // const allLinks = await getAllLinks(url);
+
   try {
-    const downloadLink = await getPage(url);
-    // console.log("chapterData", chaptersData);
-    //console.log("the json");
-    //const chaptersJson = await chaptersData.json();
-    //console.log("chapter JSON", chaptersJson);
+    const downloadLink = await getSerieDownloadLink(url);
     res.json(downloadLink);
   } catch (err) {
     res.send(`Une erreur s'est produite: ${err}`);
   }
-  /* getPage(url)
-    .then((chapter).json => res.send(chapter))
-    .catch((err) => {
-      res.send(`Une erreur s'est produite: ${err}`);
-    });*/
 });
 app.listen(port, () => {
   console.log(`this server is in port ${port}`);
 });
 
-const getPage = async (url) => {
+const getSerieDownloadLink = async (url) => {
   return new Promise(async (resolve, reject) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -61,141 +53,210 @@ const getPage = async (url) => {
       url = serieUrl;
     }
     // if url has toc path in the end eg : ?toc=11#content1
-    let urlToc =
-      /https?:\/\/www.scribblehub.com\/series\/\d{2,6}\/[\w|\W]+\/?toc=/;
-    if (urlToc.test(url)) {
-      url = url.match(urlToc)[0] + "1";
+    let serieMatch =
+      /(https?:\/\/www.scribblehub.com\/series\/\d+\/(\w|\W)*\/)\?toc=\d/;
+    if (serieMatch.test(url)) {
+      url = url.match(serieMatch)[1];
     }
     const linkToFile = await getAllLinks(url, page);
-    console.log("~ linkToFile", linkToFile);
+    // console.log("~ linkToFile", linkToFile);
+    browser.close();
     if (!linkToFile) reject("Une erreur s'est produite: Pas de chapitre.");
-    // console.log("file", file);
     resolve(linkToFile);
   });
 };
 
-const generatePDF = (file) => {
-  // Create a document
+const getAllLinks = async (url, page) => {
+  // Create a document PDF
   const doc = new PDFDocument();
 
-  // Check if downloads file exists
+  // Check if downloads folder exists
   if (!fs.existsSync("downloads"))
     fs.mkdir("downloads", (err) => {
       if (err) {
         return console.error(err);
       }
     });
+  await page.goto(url);
+  let serieName = await page.evaluate(
+    (el) => el.innerHTML,
+    await page.$(".fic_title")
+  );
+  console.log("the serie name", serieName);
 
   // Pipe its output somewhere, like to a file or HTTP response
   // See below for browser usage
-  doc.pipe(fs.createWriteStream(`./downloads/${file.serieName}.pdf`));
+  doc.pipe(
+    fs.createWriteStream(
+      `./downloads/${serieName.replace(/[^a-zA-Z0-9 ]/g, "")}.pdf`
+    )
+  );
 
-  // Embed a font, set the font size, and render some text
-  // doc
-  //   .font("fonts/PalatinoBold.ttf")
-  //   .fontSize(25)
-  //   .text("Some text with an embedded font!", 100, 100);
+  console.log("serie name : ", serieName);
 
-  // Add another page
-  // doc.addPage().fontSize(25).text("Here is some vector graphics...", 100, 100);
+  // write the book infos in the PDF Document
+  doc.font("Times-Bold").fontSize(35).text(serieName, {
+    align: "center",
+  });
 
+  doc.moveDown();
+
+  const serieImage = await page.evaluate(
+    (el) => el.src,
+    await page.$(".fic_image img")
+  );
+
+  console.log("image", serieImage);
+  const image = await fetchImage(serieImage);
+  let imageWidth = 180;
   doc
-    .text(file.serieName, 15, 15)
-    .underline(100, 100, 160, 27, { color: "#0000FF" });
-  doc.text("SYNOPSIS: ", 15, 40);
-  doc.text(
-    convert(file.description, {
-      wordwrap: 130,
-    }),
-    15,
-    75
+    .image(image, doc.page.width / 2 - imageWidth / 2, doc.y, {
+      width: imageWidth,
+    })
+    .stroke();
+  doc.moveDown(0.5);
+  addSerieInfoToPdf(
+    doc,
+    "Author Name : ",
+    await page.evaluate((el) => el.textContent, await page.$(".auth_name_fic"))
+  );
+  doc.moveDown(0.5);
+  addSerieInfoToPdf(doc, "Serie Link : ", url, 16, "blue");
+  doc.moveDown(0.5);
+  addSerieInfoToPdf(
+    doc,
+    "Author Link : ",
+    await page.evaluate(
+      (el) => el.href,
+      await page.$("span[property='name'] a")
+    ),
+    16,
+    "blue"
+  );
+  doc.moveDown(0.5);
+  addSerieInfoToPdf(
+    doc,
+    "Last Update : ",
+    await page.evaluate(
+      (el) => el.textContent,
+      await page.$(".toc_ol:first-child .fic_date_pub")
+    )
+  );
+  doc.moveDown(0.5);
+  addSerieInfoToPdf(
+    doc,
+    "number of chapters : ",
+    `${await page.evaluate(
+      (el) => el.getAttribute("order"),
+      await page.$(".toc_ol :first-child ")
+    )} chapters`
+  );
+  doc.moveDown(0.5);
+
+  doc.addPage();
+  addSerieInfoToPdf(
+    doc,
+    "SYNOPSIS : ",
+    convert(
+      await page.evaluate((el) => el.innerHTML, await page.$(".wi_fic_desc")),
+      {
+        wordwrap: 130,
+      }
+    )
   );
 
-  for (let index = 0; index < file.content.length; index++) {
-    doc.addPage();
-    doc.text(file.content[index].chapterTitle);
-    doc.moveDown();
-    doc.moveDown();
-
-    const richText = convert(file.content[index].chapterContent, {
-      wordwrap: 130,
-    });
-
-    doc.text(richText, {
-      continued: true,
-    });
-  }
-
-  // Finalize PDF file
-  doc.end();
-  return path.resolve(`./downloads/${file.serieName}.pdf`);
-};
-
-const getAllLinks = async (url, page) => {
-  await page.goto(url);
-  let file = {};
-  const tocLinks = [];
-  const serieName = await page.$(".fic_title");
-  file.serieName = await page.evaluate((el) => el.innerHTML, serieName);
-  const description = await page.$(".wi_fic_desc");
-  file.description = await page.evaluate((el) => el.innerHTML, description);
-  console.log(file.description);
-
-  tocLinks.push(
-    await page.evaluate((el) => el.href, await page.$("a[class='current']"))
-  );
-  while ((await page.$("a[class='page-link next'")) !== null) {
+  // get the url of last table of content
+  let lastTocUrl = url;
+  while ((await page.$("a[class='page-link next']")) !== null) {
     await page.goto(
       await page.evaluate(
         (el) => el.href,
-        await page.$("a[class='page-link next'")
+        await page.$("a[class='page-link next']")
       )
     );
-    tocLinks.push(
-      await page.evaluate((el) => el.href, await page.$("a[class='current']"))
+    lastTocUrl = await page.evaluate(
+      (el) => el.href,
+      await page.$("a[class='current']")
     );
   }
-  console.log("toclinks", tocLinks);
-  const chaptersList = await getListOfChapters(tocLinks);
-  const allChaptersContent = await getAllChaptersContent(chaptersList);
-  file.content = allChaptersContent;
-  const linkToFile = generatePDF(file);
-  console.log("~ linkToFile", linkToFile);
+  // get the url of the first chapter in the book
+  let firstChapterLink = await page.$("li[order='1'] a");
+  let firstChapterHref = await page.evaluate((el) => el.href, firstChapterLink);
+  console.log("href of first Chapter", firstChapterHref);
 
-  return linkToFile;
-};
-
-// get the list of all chapters in the serie;
-const getListOfChapters = async (links) => {
-  let chapterLists = [];
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  for (let i = 0; i < links.length; i++) {
-    // get Url of chapter in the toc page
-    await page.goto(links[i]);
-    const list = await page.evaluate(() =>
-      Array.from(document.querySelectorAll(".toc_a"), (element) => element.href)
+  await page.goto(firstChapterHref);
+  await addChapterContentToPdf(doc, page);
+  let index = 2;
+  while (await page.$("a[class='btn-wi btn-next']")) {
+    let nextHref = await page.evaluate(
+      (el) => el.href,
+      await page.$("a[class='btn-wi btn-next']")
     );
-    chapterLists.push(...list);
+    await page.goto(nextHref);
+    await addChapterContentToPdf(doc, page);
+    console.log(`href of index ${index} : ${nextHref}`);
+    index++;
   }
-  await browser.close();
-  return chapterLists;
+
+  console.log("end of pdf");
+  doc.end();
+
+  return path.resolve(`./downloads/${serieName}.pdf`);
 };
 
-const getAllChaptersContent = async (chapterLists) => {
-  const allChaptersContent = [];
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  for (let i = 0; i < chapterLists.length; i++) {
-    await page.goto(chapterLists[i]);
-    const chapterTitle = await page.$(".chapter-title");
-    const chapterContent = await page.$("#chp_raw");
-    const chapter = {
-      chapterTitle: await page.evaluate((el) => el.innerHTML, chapterTitle),
-      chapterContent: await page.evaluate((el) => el.innerHTML, chapterContent),
-    };
-    allChaptersContent.unshift(chapter);
-  }
-  await browser.close();
-  return allChaptersContent;
+const fetchImage = async (src) => {
+  const image = await axios.get(src, {
+    responseType: "arraybuffer",
+  });
+  return image.data;
+};
+
+const addSerieInfoToPdf = async (
+  doc,
+  boldText,
+  defaultText,
+  defaultFontSize = 16,
+  color = "black",
+  defaultFontStyle = "Times-Roman",
+  boldFontStyle = "Times-Bold"
+) => {
+  doc
+    .fontSize(defaultFontSize)
+    .font(boldFontStyle)
+    .text(boldText, {
+      continued: true,
+    })
+    .fillColor(color)
+    .font(defaultFontStyle)
+    .text(defaultText)
+    .fillColor("black");
+};
+
+const addChapterContentToPdf = async (doc, page) => {
+  let title = await getChapterTitle(page);
+  let content = await getChapterContent(page);
+  doc.addPage();
+  doc.font("Times-Bold").fontSize(18).text(title, {
+    align: "center",
+  });
+  doc.moveDown();
+  doc
+    .font("Times-Roman")
+    .fontSize(14)
+    .text(
+      convert(content, {
+        wordwrap: 130,
+      })
+    );
+};
+
+const getChapterTitle = async (page) => {
+  return await page.evaluate(
+    (el) => el.innerHTML,
+    await page.$(".chapter-title")
+  );
+};
+
+const getChapterContent = async (page) => {
+  return await page.evaluate((el) => el.innerHTML, await page.$("#chp_raw"));
 };
