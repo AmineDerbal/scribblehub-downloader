709f16684c8a7cabb531e405315a20a9c6f2fc4a
Amine derbal <aminedoran31@gmail.com>
2023-04-14 14:10:37 +0100

    refactor post url function

--- public/js/modules/url.js
+++ public/js/modules/url.js
@@ -1,6 +1,34 @@
-const testUrl = (url) => {
+import getProgress from './progress.js';
+
+export const testUrl = (url) => {
   let urlRegex = /^(https?:\/\/www\.scribblehub\.com\/(read|series)\/(\w|\W))/;
   return urlRegex.test(url);
 };
 
-export default testUrl;
+export const postNovelUrl = async (url, current) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      url: url.value,
+    }),
+  };
+  try {
+    console.log('sending data');
+    let interval = setInterval(async () => {
+      await getProgress(current);
+    }, 500);
+
+    const urlStream = await fetch('/download', options);
+    const urlData = await urlStream.json();
+    console.log('fetching data');
+    clearInterval(interval);
+    return urlData;
+  } catch (err) {
+    return { Error: err, status: 'Error' };
+  }
+};
+
+export default { testUrl, postNovelUrl };
